b7bdabef99a424a19e817fa26012ec48ac4a475c
Toshiyuki Takahagi <tosiyuki@hh.iij4u.or.jp>
2024-07-21 00:48:57 +0900

    PDFテキスト検索機能の追加
    FPDFText_FindStart.csに新しいメソッド`FPDFTextFindStart`を追加しました。このメソッドは、PDFテキストページ内で特定のテキストを検索する機能を提供します。外部の`FPDFText_FindStart`関数を呼び出すためのP/Invoke宣言が含まれており、`SuppressUnmanagedCodeSecurity`と`DllImport`属性が適用されています。検索パラメータとして、テキストページのハンドル、検索するテキスト、オプションフラグ、開始インデックスが指定され、検索コンテキストのハンドルが戻り値として返されます。必要なusingディレクティブも追加されました。

---
+++ src/PDFiumCore/FPDFText_FindStart.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Security;
+using System.Text;
+
+using __CallingConvention = global::System.Runtime.InteropServices.CallingConvention;
+using __IntPtr = global::System.IntPtr;
+
+namespace PDFiumCore
+{
+	public unsafe partial class fpdf_text
+	{
+		public partial struct __Internal
+		{
+			[SuppressUnmanagedCodeSecurity, DllImport( "pdfium", EntryPoint = "FPDFText_FindStart", CallingConvention = __CallingConvention.Cdecl )]
+			internal static extern __IntPtr FPDFTextFindStart( __IntPtr text_page, [MarshalAs(UnmanagedType.LPWStr)]string findwhat, uint flags, int start_index );
+		}
+		/// <summary>
+		/// <para>Function: FPDFText_FindStart</para>
+		/// <para>Start a search.</para>
+		/// <para>Parameters:</para>
+		/// <para>text_page   -   Handle to a text page information structure. Returned by FPDFText_LoadPage function.</para>
+		/// <para>findwhat    -   A unicode match pattern.</para>
+		/// <para>flags       -   Option flags.</para>
+		/// <para>start_index -   Start from this character. -1 for end of the page.</para>
+		/// <para>Return Value:</para>
+		/// <para>A handle for the search context. FPDFText_FindClose must be called</para>
+		/// <para>to release this handle.</para>
+		/// </summary>
+		public static global::PDFiumCore.FpdfSchhandleT FPDFTextFindStart( global::PDFiumCore.FpdfTextpageT text_page, string findwhat, uint flags, int start_index )
+		{
+			var __arg0 = text_page is null ? __IntPtr.Zero : text_page.__Instance;
+			var __ret = __Internal.FPDFTextFindStart( __arg0, findwhat, flags, start_index );
+			var __result0 = global::PDFiumCore.FpdfSchhandleT.__GetOrCreateInstance( __ret, false );
+			return __result0;
+		}
+	}
+}
