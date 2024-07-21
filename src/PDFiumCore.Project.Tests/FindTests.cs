74eb2b67662aab65040f97ef02704d86e1a857bf
Toshiyuki Takahagi <tosiyuki@hh.iij4u.or.jp>
2024-07-21 09:47:40 +0900

    テスト機能とプロジェクト更新の追加
    PDFiumライブラリのテキスト検索テスト機能とプロジェクトの更新を行いました。`FindTests.cs`と`PDFiumCore.Project.Tests.csproj`が新たに追加され、PDFのテキスト検索機能のテスト基盤が構築されました。これには、必要な名前空間の使用、テストメソッドの追加、および必要なパッケージ参照の追加が含まれます。また、`.NET 6.0`をターゲットフレームワークとする新しいテストプロジェクトファイルが作成され、`pdf-sample.pdf`ファイルが出力ディレクトリにコピーされるよう設定されました。`PDFiumCore.sln`には、Visual Studioのバージョン更新、`PDFiumCoreDemo`プロジェクトのプロジェクトタイプ変更、および`PDFiumCore.Project.Tests`プロジェクトの追加とそのビルド設定が含まれます。これらの変更により、PDFiumライブラリを使用したPDFのテキスト検索機能のテストが可能になり、プロジェクトの構成が最新の状態に更新されました。

---
+++ src/PDFiumCore.Project.Tests/FindTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDFiumCore.Project.Tests
+{
+	public class FindTests
+	{
+		[OneTimeSetUp]
+		public void OneTimeSetup()
+		{
+			fpdfview.FPDF_InitLibrary();
+		}
+		[OneTimeTearDown]
+		public void OneTimeTearDown()
+		{
+			fpdfview.FPDF_DestroyLibrary();
+		}
+		[SetUp]
+		public void Setup()
+		{
+		}
+		[Test]
+		public void Test_FindStart()
+		{
+			var document = fpdfview.FPDF_LoadDocument("pdf-sample.pdf", null);
+			Assert.That( document, Is.Not.Null );
+			var page = fpdfview.FPDF_LoadPage( document, 0 );
+			Assert.That( page, Is.Not.Null );
+			var textPage = fpdf_text.FPDFTextLoadPage( page );
+			Assert.That( textPage, Is.Not.Null );
+
+			var findHandle = fpdf_text.FPDFTextFindStart(textPage, "PDF", 0, 0);
+			Assert.That( findHandle, Is.Not.Null );
+			Assert.That( fpdf_text.FPDFTextFindNext( findHandle ), Is.Not.Zero );
+			Assert.That( fpdf_text.FPDFTextGetSchResultIndex( findHandle ), Is.EqualTo( 14 ) );
+			Assert.That( fpdf_text.FPDFTextGetSchCount( findHandle ), Is.EqualTo( 3 ) );
+
+			fpdf_text.FPDFTextFindClose(findHandle);
+			fpdfview.FPDF_ClosePage(page);
+			fpdfview.FPDF_CloseDocument(document);
+		}
+	}
+}
