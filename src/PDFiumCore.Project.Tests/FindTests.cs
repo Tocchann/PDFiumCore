bafc7f2dabc0dd885505db742172f6922cfd6140
Toshiyuki Takahagi <tosiyuki@hh.iij4u.or.jp>
2024-07-21 15:56:53 +0900

    PDFテキスト検索・抽出機能の強化
    PDF文書からテキストを検索し、抽出する機能が強化されました。FindTests.csには、新しい名前空間が追加され、デバッグトレース、プラットフォーム間の相互運用性、メディアタイプ名の使用が可能になりました。また、新しいテストメソッド`Test_GetText`が追加され、PDF文書からテキストを抽出し、そのテキストが期待通りであることを検証する機能が追加されました。既存のテストメソッドには、テキスト検索結果のインデックスとカウントを検証する新しいアサーションが追加されました。
    
    FPDFText_FindStart.csでは、不要な名前空間の使用が削除され、`FPDFText_GetText`関数のインポート宣言が追加されました。これにより、PDFページからテキストを抽出する機能が追加されました。新しい`FPDFTextGetText`メソッドは、指定された範囲のテキストをPDFページから抽出し、そのテキストを文字列として返します。テキストの長さがスタック領域を超える場合は、動的にメモリを確保してテキストを格納します。
    
    これらの変更により、PDF文書からテキストを検索し、抽出する機能が強化され、テストケースが追加され、既存の機能に対する検証が強化されています。

--- src/PDFiumCore.Project.Tests/FindTests.cs
+++ src/PDFiumCore.Project.Tests/FindTests.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace PDFiumCore.Project.Tests
 {
@@ -35,12 +38,52 @@ namespace PDFiumCore.Project.Tests
 			var findHandle = fpdf_text.FPDFTextFindStart(textPage, "PDF", 0, 0);
 			Assert.That( findHandle, Is.Not.Null );
 			Assert.That( fpdf_text.FPDFTextFindNext( findHandle ), Is.Not.Zero );
+
 			Assert.That( fpdf_text.FPDFTextGetSchResultIndex( findHandle ), Is.EqualTo( 14 ) );
 			Assert.That( fpdf_text.FPDFTextGetSchCount( findHandle ), Is.EqualTo( 3 ) );
+
+			var contextLengthOneHalf = 20;
+			var foundCharIndex = fpdf_text.FPDFTextGetSchResultIndex( findHandle );
+			var foundTextIndex = fpdf_searchex.FPDFTextGetTextIndexFromCharIndex( textPage, foundCharIndex );
+			var foundTextCount = fpdf_text.FPDFTextGetSchCount( findHandle );
+			Assert.That( foundCharIndex, Is.EqualTo( 14 ) );
+			Assert.That( foundTextIndex, Is.EqualTo( 14 ) );
+			Assert.That( foundTextCount, Is.EqualTo( 3 ) );
+
+			var charsOnPage = fpdf_text.FPDFTextCountChars( textPage );
+			int startPosition = Math.Max( foundTextIndex - contextLengthOneHalf, 0 );
+			int endPosition = Math.Min( foundTextIndex + foundTextCount + contextLengthOneHalf, charsOnPage );
+			var result = fpdf_text.FPDFTextGetText( textPage, startPosition, endPosition - startPosition, out var text );
+			Assert.That( result, Is.True );
+			Assert.That( text.Length, Is.EqualTo( endPosition - startPosition ) );
+
+			Assert.That( text.Contains( "PDF" ), Is.True );
+			Trace.WriteLine( $"Found text: {text}" );
 
 			fpdf_text.FPDFTextFindClose(findHandle);
 			fpdfview.FPDF_ClosePage(page);
 			fpdfview.FPDF_CloseDocument(document);
+		}
+		[Test]
+		public void Test_GetText()
+		{
+			var document = fpdfview.FPDF_LoadDocument( "pdf-sample.pdf", null );
+			Assert.That( document, Is.Not.Null );
+			var page = fpdfview.FPDF_LoadPage( document, 0 );
+			Assert.That( page, Is.Not.Null );
+			var textPage = fpdf_text.FPDFTextLoadPage( page );
+			Assert.That( textPage, Is.Not.Null );
+
+			var charsOnPage = fpdf_text.FPDFTextCountChars( textPage );
+			Assert.That( charsOnPage, Is.GreaterThan( 0 ) );
+			var result = fpdf_text.FPDFTextGetText( textPage, 0, charsOnPage, out var text );
+			Assert.That( result, Is.True );
+			Assert.That( text.Length, Is.EqualTo( charsOnPage ) );
+
+			Trace.WriteLine( $"Found text: {text}" );
+
+			fpdfview.FPDF_ClosePage( page );
+			fpdfview.FPDF_CloseDocument( document );
 		}
 	}
 }
--- src/PDFiumCore/FPDFText_FindStart.cs
+++ src/PDFiumCore/FPDFText_FindStart.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using System.Security;
-using System.Text;
 
 using __CallingConvention = global::System.Runtime.InteropServices.CallingConvention;
 using __IntPtr = global::System.IntPtr;
@@ -15,6 +13,9 @@ namespace PDFiumCore
 		{
 			[SuppressUnmanagedCodeSecurity, DllImport( "pdfium", EntryPoint = "FPDFText_FindStart", CallingConvention = __CallingConvention.Cdecl )]
 			internal static extern __IntPtr FPDFTextFindStart( __IntPtr text_page, [MarshalAs(UnmanagedType.LPWStr)]string findwhat, uint flags, int start_index );
+
+			[SuppressUnmanagedCodeSecurity, DllImport( "pdfium", EntryPoint = "FPDFText_GetText", CallingConvention = __CallingConvention.Cdecl )]
+			internal static extern int FPDFTextGetText( __IntPtr text_page, int start_index, int count, __IntPtr result );
 		}
 		/// <summary>
 		/// <para>Function: FPDFText_FindStart</para>
@@ -34,6 +35,49 @@ namespace PDFiumCore
 			var __ret = __Internal.FPDFTextFindStart( __arg0, findwhat, flags, start_index );
 			var __result0 = global::PDFiumCore.FpdfSchhandleT.__GetOrCreateInstance( __ret, false );
 			return __result0;
+		}
+		/// <summary>
+		/// <para>Function: FPDFText_GetText</para>
+		/// <para>Extract unicode text string from the page.</para>
+		/// <para>Parameters:</para>
+		/// <para>text_page   -   Handle to a text page information structure. Returned by FPDFText_LoadPage function.</para>
+		/// <para>start_index -   Index for the start characters.</para>
+		/// <para>count       -   Number of UCS-2 values to be extracted.</para>
+		/// <para>result      -   A buffer (allocated by application) receiving the extracted UCS-2 values. The buffer must be able to hold `count` UCS-2 values plus a terminator.</para>
+		/// <para>Return Value:</para>
+		/// <para>Number of characters written into the result buffer, including the</para>
+		/// <para>trailing terminator.</para>
+		/// <para>Comments: This function ignores characters without UCS-2 representations. It considers all characters on the page, even those that are not visible when the page has a cropbox. To filter out the characters outside of the cropbox, use FPDF_GetPageBoundingBox() and FPDFText_GetCharBox().</para>
+		/// </summary>
+		public static bool FPDFTextGetText( global::PDFiumCore.FpdfTextpageT text_page, int start_index, int count, out string result )
+		{
+			var __arg0 = text_page is null ? __IntPtr.Zero : text_page.__Instance;
+			result = string.Empty;
+			int __ret = 0;
+			var allocBuffer = __IntPtr.Zero;
+			// 使わない場合も含めて確保しておく(最適化ミスなどで消されないように確実に確保)
+			var buffer = stackalloc char[1024];
+			__IntPtr ptr = new __IntPtr( buffer );
+			// スタック領域以上の文字列を確保したい場合は、別途バッファを確保する
+			if( count >= 1024 )
+			{
+				allocBuffer = Marshal.AllocCoTaskMem( count * sizeof( char ) );
+				ptr = allocBuffer;
+			}
+			__ret = __Internal.FPDFTextGetText( __arg0, start_index, count, ptr );
+			if( __ret > 0 )
+			{
+				unsafe
+				{
+					// 終端分を含んだ長さを返すので、-1する(終端分は外側では考慮しない)
+					result = new string( (char*)ptr.ToPointer(), 0, Math.Min(count,__ret) );
+				}
+			}
+			if( allocBuffer != __IntPtr.Zero )
+			{
+				Marshal.FreeCoTaskMem( allocBuffer );
+			}
+			return __ret > 0;
 		}
 	}
 }
